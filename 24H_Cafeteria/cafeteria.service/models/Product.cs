f2e53981b11549339314534015325c08edcd5324
Klenovszky_Áron_Home <aron.klenovszky@gmail.com>
2021-10-30 17:42:22 +0200

    Create&Finish Product class

---
+++ 24H_Cafeteria/cafeteria.service/models/Product.cs
@@ -0,0 +1,29 @@
+﻿using cafeteria.data;
+
+namespace cafeteria.service.models
+{
+    public class Product : IProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public bool IsDeleted { get; set; } = false;
+
+        public Product()
+        { }
+
+        public Product(string row)
+        {
+            var data = row.Split(";");
+            Id = int.Parse(data[0]);
+            Name = data[1];
+            Price = int.Parse(data[2]);
+            IsDeleted = data[3] == "True";
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0};{1};{2};{3}", Id, Name, Price, IsDeleted);
+        }
+    }
+}
