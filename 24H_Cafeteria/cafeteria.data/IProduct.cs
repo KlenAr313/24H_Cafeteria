3938204543e56ecdd380c210dd31f77a35665497
Klenovszky_Áron_Home <aron.klenovszky@gmail.com>
2021-10-30 13:23:40 +0200

    Create&Fninish IProduct
    Name Price Isdeleted

---
+++ 24H_Cafeteria/cafeteria.data/IProduct.cs
@@ -0,0 +1,9 @@
+﻿namespace cafeteria.data
+{
+    public interface IProduct : IIdentity
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
