4535cc33ace902114264e4c25f7ca0fdccebb35e
Klenovszky_Áron_Home <aron.klenovszky@gmail.com>
2021-10-30 13:20:47 +0200

    Create&Finish IOrder
    Orderer; OrderTimeStamp; Product(DIctionary); Comment; IsFinished

---
+++ 24H_Cafeteria/cafeteria.data/IOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cafeteria.data
+{
+    public interface IOrder : IIdentity
+    {
+        string Orderer { get; set; }
+        DateTime OrderTimeStamp { get; set; }
+        Dictionary<IProduct, int> Products { get; set; }
+        string Comment { get; set; }
+        bool? IsFinished { get; set; }
+    }
+}
