bc92756e7b8a1de7b5ff2f16be38d4ccdee0ab38
Klenovszky_Áron_Home <aron.klenovszky@gmail.com>
2021-10-30 18:56:20 +0200

    Create Product repo class

---
+++ 24H_Cafeteria/cafeteria.service/repositorys/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using cafeteria.service.models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace cafeteria.service.repositorys
+{
+    public class ProductRepository
+    {
+        readonly string fileName;
+
+        public ProductRepository(string fileName = "products.csv")
+        {
+            this.fileName = fileName;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            List<Product> products = new List<Product>();
+            StreamReader sr = new StreamReader(fileName);
+            while (!sr.EndOfStream)
+            {
+                products.Add(new Product(sr.ReadLine()));
+            }
+            sr.Close();
+            return products;
+        }
+
+        public Product Create(Product product)
+        {
+            var allItems = GetAll();
+            int maxId = 0;
+            if (allItems.Count() > 0)
+            {
+                maxId = allItems.Max(e => e.Id);
+            }
+            product.Id = maxId + 1;
+            using (StreamWriter sw = new StreamWriter(fileName, true))
+            {
+                sw.WriteLine(product);
+                sw.Close();
+            }
+            return product;
+        }
+    }
+}
