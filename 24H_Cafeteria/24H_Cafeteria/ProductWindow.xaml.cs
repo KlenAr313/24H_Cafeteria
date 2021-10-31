d1d80eb2f84cbb23111348dbffc8d12cea08f0fe
Klenovszky_Áron_Home <aron.klenovszky@gmail.com>
2021-10-31 09:25:29 +0100

    MainW. menu + ProductW.
    Menu with a menu item that opens product window where we willbe able to manage products

--- 24H_Cafeteria/24H_Cafeteria/ProductWindow.xaml.cs
+++ 24H_Cafeteria/24H_Cafeteria/ProductWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,25 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace _24H_Cafeteria
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ProductWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ProductWindow : Window
     {
-        public MainWindow()
+        public ProductWindow()
         {
             InitializeComponent();
-        }
-
-        private void miProduct_Click(object sender, RoutedEventArgs e)
-        {
-            var productWindow = new ProductWindow();
-            productWindow.ShowDialog();
         }
     }
 }
